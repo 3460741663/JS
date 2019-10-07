17c8f94d3aa91afb62928479d35dde52024caf49
3460741663 <3460741663@qq.com>
2019-10-07 14:03:11 +0800

    fjdk

---
+++ NeteaseCloudMusic/src/components/header.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="header">
+    <div class="header-icon" @click="leftEvent">
+      <slot name="left-icon"></slot>
+    </div>
+    <div class="header-cont">
+      <slot name="content"></slot>
+    </div>
+    <div class="header-right">
+      <slot name="right-icon"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'header',
+  data () {
+    return {}
+  },
+  methods: {
+    leftEvent () {
+      this.$store.dispatch('setShowSiderbar', true)
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '../assets/css/function.styl'
+  .header
+    height px2rem(88px)
+    line-height px2rem(88px)
+    text-align center
+    display flex
+    align-items center
+    justify-content center
+    color #746ca8
+    font-size px2rem(30px)
+    &-icon
+      flex 0 0 px2rem(88px)
+      margin-top px2rem(6px)
+      cursor pointer
+      .icon
+        font-size px2rem(48px)
+    &-cont
+      flex 1
+      text-align center
+      font-size 18px
+      color #fff
+      font-weight 500
+      overflow hidden
+      text-overflow ellipsis
+      white-space nowrap
+</style>
