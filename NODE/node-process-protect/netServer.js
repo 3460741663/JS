86c213dca9230ab450351d496cdd10abe1dc15f9
3460741663 <3460741663@qq.com>
2019-11-10 22:08:42 +0800

    进程保护

---
+++ NODE/node-process-protect/netServer.js
@@ -0,0 +1,19 @@
+const net = require('net')
+const http = require('http')
+const server = net.createServer()
+const httpServer = http.createServer((req,res) => {
+  res.end('i am from http');
+  throw new Error('error')
+})
+
+server.on('connection',(socket) => {
+  // socket.on('data',(data) => {
+  //   console.log('server rec:',data)
+  //   socket.write('hi')
+  // })
+  httpServer.emit('connection',socket)
+})
+
+server.listen(8181,() => {
+  console.log('server is run 8181')
+})
