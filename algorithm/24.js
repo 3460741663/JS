68242d45c701f16cde08849cfc8b54808c27b4f9
3460741663 <3460741663@qq.com>
2019-12-11 23:43:47 +0800

    剑指offer

--- algorithm/24.js
+++ algorithm/24.js
@@ -5,36 +5,39 @@
 } */
 function FindPath(root, expectNumber)
 {
-  // write code here
-}
-// 先序遍历
-function preTrace(head){
+  // 初始化结果
+  let item = [];
   let result = [];
-  let stack = [];
-  stack.push(head);
-  while(stack.length){
-    // console.log(stack.length);
-    let temp = stack.pop();
-    if(temp){
-      result.push(temp.val);
-      temp.right && stack.push(temp.right)
-      temp.left && stack.push(temp.left)
-    }
-  }
+  findPath(root, expectNumber, item, result);
   return result;
+}
+function findPath(root, expectNumber, item, result){
+  if(!root) return;
+  // 刚好等于整数且为根节点
+  if(root.val === expectNumber && !root.right && !root.left) {
+    item.push(root.val);
+    result.unshift(item); // 保存如结果数组
+  }
+  if(root.val < expectNumber){
+    item.push(root.val)
+    root.right && findPath(root.right, expectNumber - root.val, item.slice(0, item.length), result);
+    root.left && findPath(root.left, expectNumber - root.val, item.slice(0, item.length), result);
+  }
 }
 var tree ={
   val: 2,
   left: {
     val: 4,
     left: {
-      val: 0
+      val: 1
     },
     right: {
       val: 7
     }
   },
   right: {
-    val: 5
+    val: 11
   }
 }
+console.log(FindPath(tree, 13))
+
